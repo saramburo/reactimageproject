a2d92f9763dd504bd287fe8e22eaac50829f4301
saramburo <106128587+saramburo@users.noreply.github.com>
2023-10-25 20:42:19 -0500

    first files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ImageContainer } from './ImageContainer'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [arrayImage, setArrayImage] = [
+    {
+      id: 1,
+      url: "https://images.secretlab.co/theme/common/collab_pokemon_catalog_charizard-min.png",
+      name: "Charizard",
+    },
+    {
+      id: 2,
+      url: "https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-6895600_960_720.png",
+      name: "Charmander",
+    },
+    {
+      id: 3,
+      url: "https://s.yimg.com/ny/api/res/1.2/TLxQ82SAuWxf6uwg2e4WCg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/es/levelup_525/dba23de2487ea6e4f41d244e1e982267",
+      name: "Tortuga"
+    },
+  ];
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ImageContainer arrayImage={arrayImage} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/ImageContainer.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React,{useState,useEffect} from "react";
+
+export function ImageContainer({arrayImage}){
+
+    return(
+        <>
+       
+                  <>
+                  <p>fasdfasd</p>
+                   
+                  </>
+                     
+
+        
+            
+        
+        </>
+    )
+}
